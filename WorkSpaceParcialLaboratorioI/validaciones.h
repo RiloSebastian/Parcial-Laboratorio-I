18b7b7706c9f379d78ea87d8dc0abdbf46d06e9d
S3baRr00 <36234060+S3baRr00@users.noreply.github.com>
2018-05-16 23:43:49 -0300

    Add files via upload

---
+++ WorkSpaceParcialLaboratorioI/validaciones.h
@@ -0,0 +1,7 @@
+#ifndef VALIDACIONES_H_INCLUDED
+#define VALIDACIONES_H_INCLUDED
+
+char validarnombre(char input[]);
+char validarNumerochar(char numero1[]);
+int validarNumero(int numero);
+#endif
